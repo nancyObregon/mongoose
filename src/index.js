859d799627debc191a64f9222aaa6fd6f9606bbd
Nancy Nallely Obregón Espinoza <nancy.obregon@softtek.com>
2021-06-14 21:36:44 -0500

    Add new function for names

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import { fifteen } from './helpers'
 
-import { dates } from './utils/inventors.utils'
+import { dates, names } from './utils/inventors.utils'
 
 const inventors = [
 	{ first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
@@ -17,6 +17,13 @@ const inventors = [
 	{ first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
 ]
 
+const inventors = [
+	{ first: 'Nancy', last: 'Obregon', year: 1879, passed: 1955 },
+	{ first: 'Daniela', last: 'Estrada', year: 1643, passed: 1727 },
+	{ first: 'Alex', last: 'Galaz', year: 1564, passed: 1642 },
+	{ first: 'Luis', last: 'Curie', year: 1867, passed: 1934 }
+]
+
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 const fifteen = inventors.filter((inventor) => inventor.year >= 1500 && inventor.year < 1600)
@@ -30,11 +37,14 @@ console.log(resultFitteen)
 // Array.prototype.map()
 // 2. Give us an array of the inventor first and last names
 // deconstruyendo las propiedades para tener a mismo nivel
-// const fullNames = inventors.map(({ first, last }) => `${first} ${last}`)
-// fullNames
+const fullNames = inventors.map(({ first, last }) => `${first} ${last}`)
+console.log(fullNames)
+
+const fullNames1 = names(inventors)
+console.log(fullNames1)
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
-// const totalYears = inventors.reduce((total, inventor) => {
-// 	return total + (inventor.passed - inventor.year)
-// }, 0)
+const totalYears = inventors.reduce((total, inventor) => {
+	return total + (inventor.passed - inventor.year)
+}, 0)
--- src/utils/inventors.utils.js
+++ src/utils/inventors.utils.js
@@ -1,3 +1,5 @@
 export const dates = (list, startDate, endDate) => {
 	return list.filter((row) => row.year >= startDate && row.year < endDate)
 }
+
+export const names = (list) => list.map(({ first, last }) => `${first} ${last}`)
